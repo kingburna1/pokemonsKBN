02c18d5d4e26c4c7abac95c06c570b5257f3a2f5
burna money <agborboniventure@gmail.com>
2025-05-05 13:48:12 +0100

    update4

--- src/app/pokemon/[id]/page.jsx
+++ src/app/pokemon/[id]/page.jsx
@@ -6,7 +6,50 @@ import NavbarComponent from './../../../components/navbar/NavbarComponent';
 import FooterComponent from './../../../components/footer/FooterComponent';
 import HeroComponent from './../../../components/hero/HeroComponent';
 import AddcontComponent from '../../../components/addcont/AddcontComponent';
+import { Image } from 'next/image';
 
+export async function generateMetadata({ params }) {
+  const { id } = params;
+  const pokedata = Pokemons.find(item => item.id == id);
+
+  if (!pokedata) {
+    return {
+      title: "Pokémon Not Found",
+    };
+  }
+
+  return {
+    title: `${pokedata.name} - Pokémon Explorer`,
+    description: `Discover information about ${pokedata.name}, including stats, types, and more.`,
+    icons: {
+      icon: pokedata.image,
+      apple: pokedata.image,
+      shortcut: pokedata.image,
+    },
+    openGraph: {
+      title: `${pokedata.name} - Pokémon Explorer`,
+      description: `Explore detailed information about ${pokedata.name} in our modern Pokédex.`,
+      url: `${pokedata.image}`,
+      siteName: "Pokémon Explorer",
+      images: [
+        {
+          url: pokedata.image,
+          width: 1200,
+          height: 630,
+          alt: `${pokedata.name} Pokémon`,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${pokedata.name} - Pokémon Explorer`,
+      description: `Explore ${pokedata.name} on Pokémon Explorer powered by PokéAPI.`,
+      images: [pokedata.image],
+    },
+  };
+}
 
 const Page = async ({ params }) => {
   const {id} = await params;
